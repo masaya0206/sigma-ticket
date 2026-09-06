b99b9135b2aacc9fb3bc9e8073a055d35c027c1b
masaya0206 <yashiheimu41@gmail.com>
2026-09-06 20:11:47 +0900

    Update cache name and modify app shell files

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sigma-ticket-pwa-v7-access1";
+const CACHE_NAME = "sigma-ticket-pwa-v75";
 const APP_SHELL = [
  "./",
  "./home.html",
@@ -14,8 +14,6 @@ const APP_SHELL = [
  "./seller-team.html",
  "./points-admin.html",
  "./points-my.html",
-  "./presale-admin.html",
-  "./presale.html",
  "./order-entry.html",
  "./register-teams.html",
  "./shopping.html",
@@ -23,11 +21,9 @@ const APP_SHELL = [
  "./manifest.webmanifest",
  "./icon-180.png",
  "./icon-192.png",
- "./icon-512.png"
-,
-  "./shift.html",
-  "./attendance-qr.html",
-  "./event-admin.html"];
+ "./icon-512.png",
+ "./icon-maskable-512.png"
+];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{const r=e.request;if(r.method!=="GET")return;const u=new URL(r.url);if(u.origin!==location.origin)return;if(r.mode==="navigate"||r.destination==="document"){e.respondWith(fetch(r).then(res=>{const cp=res.clone();caches.open(CACHE_NAME).then(c=>c.put(r,cp));return res}).catch(()=>caches.match(r).then(x=>x||caches.match("./home.html"))));return;}e.respondWith(caches.match(r).then(c=>c||fetch(r).then(res=>{const cp=res.clone();caches.open(CACHE_NAME).then(x=>x.put(r,cp));return res})));});
