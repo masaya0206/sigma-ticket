04d5f961be304848f85a65fdc32733e839e4f7ee
masaya0206 <yashiheimu41@gmail.com>
2026-09-16 20:12:23 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sigma-ticket-ios-native-date-overflow-v3";
+const CACHE_NAME = "sigma-ticket-partner-invite-one-time-v1";
 self.addEventListener("install",e=>e.waitUntil(self.skipWaiting()));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
