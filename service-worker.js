5adcf1881ebddd453abea17f62d227a4f4c609ba
masaya0206 <yashiheimu41@gmail.com>
2026-09-16 19:38:42 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sigma-ticket-admin-menu-restore-v1";
+const CACHE_NAME = "sigma-ticket-ios-native-date-overflow-v3";
 self.addEventListener("install",e=>e.waitUntil(self.skipWaiting()));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
