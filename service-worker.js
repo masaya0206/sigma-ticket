1f2eb09e99db389e810ee6fdfd181fdb76fa9b85
masaya0206 <yashiheimu41@gmail.com>
2026-09-17 03:01:53 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sigma-ticket-worktime-day-role-filters-v1";
+const CACHE_NAME = "sigma-ticket-sales-product-summary-v1";
 self.addEventListener("install",e=>e.waitUntil(self.skipWaiting()));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
