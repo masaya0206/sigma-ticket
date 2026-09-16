5877a7da46433859305d9d64edd411768a3f1a23
masaya0206 <yashiheimu41@gmail.com>
2026-09-16 18:54:48 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sigma-ticket-admin-price-shift-cleanup-v1";
+const CACHE_NAME = "sigma-ticket-menu-cleanup-manager-layout-v1";
 self.addEventListener("install",e=>e.waitUntil(self.skipWaiting()));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
