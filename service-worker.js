4ad80118a2e32ce8d718cc4bf0e45778941df2af
masaya0206 <yashiheimu41@gmail.com>
2026-09-16 16:42:22 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sigma-ticket-pwa-presale-shift-v1";
+const CACHE_NAME = "sigma-ticket-pwa-presale-transfer-roleprefs-v1";
 self.addEventListener("install",e=>e.waitUntil(self.skipWaiting()));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
